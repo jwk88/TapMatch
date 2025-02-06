d4c424057b6e5d460a56d9eb197de1f5a494edc1
Jani Konttinen <jwk88@protonmail.com>
2025-02-06 18:14:31 +0200

    Add the positioning and rendering implementations for the views

--- Assets/Scripts/Views/CellView.cs
+++ Assets/Scripts/Views/CellView.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace MyTapMatch
@@ -6,29 +8,94 @@ namespace MyTapMatch
     {
         [SerializeField] SpriteRenderer _renderer;
 
-        public Cell Cell { get => _cell; private set => _cell = value; }
-        public Vector3 GamePosition { get => _gamePosition; private set => _gamePosition = value; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public bool Dirty { get; set; }
+        public Vector3 WorldPosition { get => _worldPosition; private set => _worldPosition = value; }
         public SpriteRenderer Renderer { get => _renderer; private set => _renderer = value; }
 
-        Cell _cell;
-        Vector3 _gamePosition;
+        Vector3 _worldPosition;
+        int _gameWidth;
+        int _gameHeight;
+        int _size;
+        float _spacing;
 
-        public void Initialize(Cell cell, int gameWidth, int gameHeight, int size, float spacing)
+        public virtual void Initialize(Cell cell, int gameWidth, int gameHeight, int size, float spacing, float yOffset)
         {
-            Cell = cell;
-            var cellPos = cell.Vector3();
+            _gameWidth = gameWidth;
+            _gameHeight = gameHeight;
+            _spacing = spacing;
+            _size = size;
+
+            X = cell.X;
+            Y = cell.Y;
+
+            var vec3 = cell.Vector3();
+            
             var center = new Vector2(gameWidth / 2, gameHeight / 2);
-            cellPos.x -= center.x;
-            cellPos.y -= center.y;
-            cellPos.x *= size + spacing;
-            cellPos.y *= size + spacing;
+            vec3.x -= center.x;
+            vec3.y -= center.y;
+            vec3.x *= size + spacing;
+            vec3.y *= size + spacing;
+
             transform.localScale = Vector3.one * size;
-            GamePosition = cellPos;
+            transform.position = vec3 + (Vector3.up * yOffset);
+            
+            WorldPosition = vec3;
         }
 
-        public void Render()
+        public virtual void UpdatePosition(Cell cell)
         {
-            transform.position = GamePosition;
+            X = cell.X;
+            Y = cell.Y;
+
+            var vec3 = cell.Vector3();
+            
+            var center = new Vector2(_gameWidth / 2, _gameHeight / 2);
+            vec3.x -= center.x;
+            vec3.y -= center.y;
+            vec3.x *= _size + _spacing;
+            vec3.y *= _size + _spacing;
+
+            transform.localScale = Vector3.one * _size;
+            WorldPosition = vec3;
+        }
+
+        public IEnumerator Lerp(Transform transform, Vector3 a, Vector3 b, EasingFunction.Ease easing, float speed = 1f)
+        {
+            var t = 0f;
+            while (t < 1f)
+            {
+                t += Time.deltaTime * speed;
+                var easeFunction = EasingFunction.GetEasingFunction(easing);
+                var eased = easeFunction(0, 1, t);
+                transform.position = Vector3.Lerp(a, b, eased);
+                yield return null;
+            }
+
+            transform.position = b;
+        }
+
+        public static HashSet<T> CreateViews<T>(GameClient _client, Grid gameGrid, T prefab, Transform parent = null) where T : CellView
+        {   
+            var created = new HashSet<T>();
+            foreach (var cell in gameGrid)
+            {
+                var view = GameObject.Instantiate(prefab, parent);
+
+                var width = _client.GameWidth;
+                var height = _client.GameHeight;
+                var size = _client.CellSize;
+                var spacing = _client.GridSpacing;
+                var yOffset = _client.GridCellStartOffsetY;
+                
+                view.Initialize(cell, width, height, size, spacing, yOffset);
+                view.Dirty = true;
+
+                created.Add(view);
+            }
+
+            return created;
         }
     }
 }
--- Assets/Scripts/Views/PlayableView.cs
+++ Assets/Scripts/Views/PlayableView.cs
@@ -4,6 +4,9 @@ namespace MyTapMatch
 {
     public class PlayableView : CellView 
     { 
-        
+        public void SetColor(Cell cell)
+        {
+            Renderer.color = new Color(cell.r, cell.g, cell.b, cell.a);
+        }  
     }
 }
